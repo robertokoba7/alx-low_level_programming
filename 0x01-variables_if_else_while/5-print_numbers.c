873d1c22eff36bdff4bc7849176aa01dfefcacfb
robertokoba7 <okobarobert7@gmail.com>
2022-06-03 08:58:58 -0700

    Single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - single digit numbers of base 10
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		printf("%d", k);
+	}
+	putchar('\n');
+	return (0);
+}
