e519413c3548db1b7c2feaa320c6acab58bb0509
robertokoba7 <okobarobert7@gmail.com>
2022-06-03 07:29:25 -0700

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print lower case alphabets
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int a;
+
+	for (a = 0; a < 26; a++)
+	{
+		putchar(alph[a]);
+	}
+	putchar('\n');
+	return (0);
+}
