07fbbec3a08cd46e88279685f1d696ff0804f4be
robertokoba7 <okobarobert7@gmail.com>
2022-06-03 14:43:50 -0700

    numbers of base 16 in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int n;
 
-	for (n = 10; n < 20; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 	}
-	for (n = 22; n < 28; n++)
+	for (n = 97; n < 103; n++)
 	{
 		putchar(n);
 	}
