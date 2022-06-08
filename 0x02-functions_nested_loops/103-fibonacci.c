e6a15b14ee917a23f9d2d243fe12691cb288c173
robertokoba7 <okobarobert7@gmail.com>
2022-06-08 04:27:12 -0700

    103-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio>
+/**
+ * main - Prints the add of the even-valued fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int l1, l2, fl, afl;
+	l1 = 1;
+	l2 = 2;
+	fl = afl = 0;
+	while (fl <= 4000000)
+	{
+		fl = l1 + l2;
+		l1 = l2;
+		l2 = fl;
+		if ((l1 % 2) == 0)
+		{
+			afl += l1;
+		}
+	}
+	printf("%ld\n", afl);
+	return (0);
+}
