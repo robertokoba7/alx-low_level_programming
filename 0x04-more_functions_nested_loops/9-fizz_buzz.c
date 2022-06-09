634b7ab12949fe83b88b385b75cb8eba3484c7c1
robertokoba7 <okobarobert7@gmail.com>
2022-06-09 09:58:49 -0700

    The “Fizz-Buzz test”

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints Buzz each numbers of 3 and 5
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	x = 1;
+	printf("%d", x);
+	for (x = 2; x <= 100; x++)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", x);
+		}
+	}
+	printf("\n");
+	return (0);
+}
