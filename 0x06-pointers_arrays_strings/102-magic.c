11375efb9302e0a72715a3061f161a887e6d19c6
robertokoba7 <okobarobert7@gmail.com>
2022-06-15 13:41:38 -0700

    Magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - does magic :)
- * Return: 0
- */
 int main(void)
 {
 	int n;
@@ -23,6 +19,5 @@ int main(void)
 	*(p + 5) = 98;
 	/*...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
-
 	return (0);
 }
