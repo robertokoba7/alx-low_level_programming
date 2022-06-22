ff795df9c52ba95fe1c60aeec43d41686a883bbd
robertokoba7 <okobarobert7@gmail.com>
2022-06-21 17:21:36 -0700

    returns the value of x^y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y.
+ *
+ * @x: base value.
+ * @y: power.
+ * Return: -1 if y is lower than 0, returns x**y otherwise
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
+
