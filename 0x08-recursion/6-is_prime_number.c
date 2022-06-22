75070632a39f24236bbe23d3157f751d94e02437
robertokoba7 <okobarobert7@gmail.com>
2022-06-21 18:13:51 -0700

    Pime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,19 +3,19 @@
 /**
  * is_prime -wrapper function to check for prime.
  * @n: input number.
- * @i: factor to check if n is divisible.
+ * @m: factor to check if n is divisible.
  * Return: 1 if prime, 0 if not else recurse.
  */
-int is_prime(int n, int c)
+int is_prime(int n, int m)
 {
-	if (n % c == 0)
+	if (n % m == 0)
 	{
-		if (n == c)
+		if (n == m)
 			return (1);
 		else
 			return (0);
 	}
-	return (0 + is_prime(n, c + 1));
+	return (0 + is_prime(n, m + 1));
 }
 /**
  * is_prime_number - checks if input integer is prime number.
