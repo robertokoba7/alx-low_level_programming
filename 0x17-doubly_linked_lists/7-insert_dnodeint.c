80aa825914e8f12299ac413cb3f93721865d323d
robertokoba7 <okobarobert7@gmail.com>
2022-09-01 17:58:38 +0300

    insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
  * @n: value of the new node
  * Return: the address of the new node, or NULL if it failed/
  */
- dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
 	dlistint_t *head;
@@ -50,6 +50,5 @@
 			i++;
 		}
 	}
-
 	return (new);
 }
