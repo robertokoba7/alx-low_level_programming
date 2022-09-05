b23d7f93aa6c32d2e302229bdb532bc95ed12367
robertokoba7 <okobarobert7@gmail.com>
2022-09-05 20:57:54 +0300

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/robertokoba7/alx-low_level_programming/main/0x18-dynamic_libraries/preload.so
+wget -P /tmp/ https://raw.github.com/robertokoba7/\
+	alx-low_level_programming/main/0x18-dynamic_libraries/preload.so
 export LD_PRELOAD=/tmp/preload.so
